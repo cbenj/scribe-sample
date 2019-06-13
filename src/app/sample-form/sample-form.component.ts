7f7bee39d25a08b2a645169fe9e6a074ca6c984c
Niko1316 <nicolas.vw@hotmail.fr>
2019-06-13 12:09:43 +0200

    add listen

--- src/app/sample-form/sample-form.component.ts
+++ src/app/sample-form/sample-form.component.ts
@@ -1,85 +1,106 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import Speech from "speak-tts";
-import {SampleForm} from "./sample-form";
-import {ScribeComponent} from "../scribe-component/scribe.component";
+import { SampleForm } from "./sample-form";
+import { resultList, RxSpeechRecognitionService } from "@kamiazya/ngx-speech-recognition";
 
 @Component({
-    selector: 'app-sample-form',
-    templateUrl: './sample-form.component.html',
-    styleUrls: ['./sample-form.component.css']
+  selector: 'app-sample-form',
+  templateUrl: './sample-form.component.html',
+  styleUrls: ['./sample-form.component.css'],
+  providers: [
+    RxSpeechRecognitionService,
+  ]
 })
 export class SampleFormComponent {
 
-    sampleForm: SampleForm;
-    scribeEnabled: boolean;
-    innovative = ["Yes", "No", "Maybe"]
-    player: any
+  sampleForm: SampleForm;
+  scribeEnabled: boolean;
+  innovative = ["Yes", "No", "Maybe"]
+  player: any
 
-    constructor() {
-        this.player = new Speech();
-        this.player
-            .init({
-                volume: 0.5,
-                lang: "en-GB",
-                rate: 1,
-                pitch: 1,
-                //'voice':'Google UK English Male',
-                //'splitSentences': false,
-                listeners: {
-                    onvoiceschanged: voices => {
-                        console.log("Voices changed", voices);
-                    }
-                }
-            });
-        this.sampleForm = new SampleForm();
-        this.scribeEnabled = false;
-    }
-
-    projectNameQuestion() {
-        this.player.speak({
-            text: 'What is your project name ?'
-        });
-    }
-
-    innovativeQuestion() {
-        this.player.speak({
-            text: 'Are you really innovative?'
-        });
-    }
-
-    valuePropositionQuestion() {
-        this.player.speak({
-            text: 'What is your proposition of value ?'
-        });
-    }
-
-    startSolutionQuestion() {
-      this.player.speak({
-        text: 'When did you start to imagine the solution ?'
-      });
-    }
-
-    customersIdentifiedQuestion() {
-      this.player.speak({
-        text: 'Which customers have you identified ?'
-      });
-    }
-
-    useSolutionQuestion() {
-      this.player.speak({
-        text: 'Who will use the solution ?'
-      });
-    }
-
-    submit() {
-        window.alert('submit !');
-    }
-
-    enableScribe() {
-        if (!this.scribeEnabled) {
-            this.scribeEnabled = true;
+  constructor(public service: RxSpeechRecognitionService) {
+    this.player = new Speech();
+    this.player
+      .init({
+        volume: 0.5,
+        lang: "en-GB",
+        rate: 1,
+        pitch: 1,
+        //'voice':'Google UK English Male',
+        //'splitSentences': false,
+        listeners: {
+          onvoiceschanged: voices => {
+            console.log("Voices changed", voices);
+          }
         }
+      });
+    this.sampleForm = new SampleForm();
+    this.scribeEnabled = false;
+  }
+
+  projectNameQuestion() {
+    this.player.speak({
+      text: 'What is your project name ?'
+    });
+    this.listen("projectName");
+  }
+
+  innovativeQuestion() {
+    this.player.speak({
+      text: 'Are you really innovative?'
+    });
+    this.listen("innovative");
+  }
+
+  valuePropositionQuestion() {
+    this.player.speak({
+      text: 'What is your proposition of value ?'
+    });
+    this.listen("valueProposition");
+  }
+
+  startSolutionQuestion() {
+    this.player.speak({
+      text: 'When did you start to imagine the solution ?'
+    });
+    this.listen("imagineSolution");
+  }
+
+  customersIdentifiedQuestion() {
+    this.player.speak({
+      text: 'Which customers have you identified ?'
+    });
+    this.listen("customersIdentified");
+  }
+
+  useSolutionQuestion() {
+    this.player.speak({
+      text: 'Who will use the solution ?'
+    });
+    this.listen("useSolution");
+  }
+
+  listen(fieldname: string) {
+    if (this.scribeEnabled) {
+      this.service
+        .listen()
+        .pipe(resultList)
+        .subscribe((list: SpeechRecognitionResultList) => {
+          this.sampleForm[fieldname] = list.item(0).item(0).transcript;
+          console.log('RxComponent:onresult', this.sampleForm[fieldname], list);
+        });
     }
+  }
+
+  submit() {
+    window.alert('submit !');
+  }
+
+  enableScribe() {
+    if (!this.scribeEnabled) {
+      this.scribeEnabled = true;
+    }
+  }
 
 }
 
